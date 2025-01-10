e49419ae523c2dad18ac890fbeea3f9890fc8b7d
Manoel Neto <mneto_004@hotmail.com>
2025-01-10 13:02:36 -0300

    feat: create footer component

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className='pr-4 pt-0 h-10'>
+      <span className='text-secondary-main flex justify-end items-center font-semibold'>
+        Project developed by Manoel Neto
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
