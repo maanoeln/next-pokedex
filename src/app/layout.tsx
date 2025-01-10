86e2bae0a02be468e1bcf82376b2f1616ec68e3e
Manoel Neto <mneto_004@hotmail.com>
2025-01-10 13:03:19 -0300

    feat: add header and footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Pokedex',
@@ -16,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <div className='h-full flex flex-col gap-4 bg-primary-dark dark:bg-primary-dark'>
+        <div className='min-h-screen h-screen flex flex-col bg-primary-dark dark:bg-primary-dark'>
           <Header />
-          <main className='h-full border'>{children}</main>
+          <main className='flex-grow overflow-hidden'>{children}</main>
           <Footer />
         </div>
       </body>
