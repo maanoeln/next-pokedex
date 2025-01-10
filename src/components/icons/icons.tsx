05296c66c37274d413a13ff3a1b2d9bd9a0c6919
Manoel Neto <mneto_004@hotmail.com>
2025-01-10 03:44:04 -0300

    feat: create icons component

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,43 @@
+import icons from '@/assets/icons';
+import { SVGProps } from 'react';
+
+interface IIcons
+  extends Pick<
+    SVGProps<SVGElement>,
+    'strokeLinecap' | 'strokeLinejoin' | 'strokeWidth' | 'fill' | 'stroke'
+  > {
+  name: string;
+  className: string;
+  viewbox?: string;
+  width?: string;
+  height?: string;
+}
+
+function Icons({
+  name,
+  className,
+  viewbox,
+  width,
+  height,
+  fill,
+  stroke,
+  strokeWidth,
+  ...props
+}: IIcons) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={viewbox || '0 0 16 16'}
+      width={width || '24px'}
+      height={height || '24px'}
+      className={className}
+      fill={fill}
+      strokeWidth={strokeWidth}
+      stroke={stroke}
+    >
+      <path d={icons[name]} {...props} />
+    </svg>
+  );
+}
+
+export default Icons;
