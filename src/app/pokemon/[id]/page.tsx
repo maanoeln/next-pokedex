9cf3be7707fc6722dc507c8db4eeeba28658cc90
Manoel Neto <mneto_004@hotmail.com>
2025-01-16 06:49:32 -0300

    feat: split static from dynamic

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,60 +1,16 @@
-'use client';
-
-import getImageUrl from '@/helpers/getImageUrl';
-// import getWavesColor from '@/helpers/getWavesColor';
-import { typesLight } from '@/helpers/typesColors';
-import { useFetchApi } from '@/hooks/useFetchApi';
-import { PokemonId } from '@/interfaces/pokemons';
-import Image from 'next/image';
-import { useParams } from 'next/navigation';
+import PokemonDetailsClient from '@/app/pokemon/[id]/pokemonDetailsClient';
 
 export async function generateStaticParams() {
-  // Fetch a list of Pokémon IDs (replace with your API or static data)
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+  // Fetch Pokémon IDs from an API or static list
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
   const data = await response.json();
 
-  // Map the Pokémon data to an array of params
   return data.results.map((pokemon: { url: string }) => {
     const id = pokemon.url.split('/').filter(Boolean).pop(); // Extract ID from URL
     return { id };
   });
 }
 
-function PokemonDetails() {
-  const { id } = useParams<{ id: string }>();
-  const { data } = useFetchApi<PokemonId>({
-    url: `/pokemon/${id}`,
-    initialData: { id: 0, name: '', url: '', sprites: '', types: [] },
-  });
-
-  const light = typesLight[data.types[0]?.type.name];
-
-  return (
-    <div
-      className='flex justify-start my-4 bg-waves bg-contain bg-no-repeat h-full w-[70%] m-auto capitalize'
-      style={{
-        backgroundPosition: 'bottom',
-      }}
-    >
-      <div className='flex justify-center flex-col'>
-        <span className='text-2xl text-secondary-light'>{data.name}</span>
-        <div className='relative flex justify-start items-center'>
-          <div
-            className='absolute w-[300px] h-[300px] rounded-full'
-            style={{ backgroundColor: light }}
-          ></div>
-          <Image
-            className='z-20 translate'
-            src={getImageUrl(+id)}
-            width={400}
-            height={400}
-            alt={data.name}
-          />
-        </div>
-      </div>
-      <div>Info here</div>
-    </div>
-  );
+export default function PokemonPage({ params }: { params: { id: string } }) {
+  return <PokemonDetailsClient id={params.id} />;
 }
-
-export default PokemonDetails;
---
+++ src/app/pokemon/[id]/pokemonDetailsClient.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import getImageUrl from '@/helpers/getImageUrl';
+import { typesLight } from '@/helpers/typesColors';
+import { useFetchApi } from '@/hooks/useFetchApi';
+import { PokemonId } from '@/interfaces/pokemons';
+import Image from 'next/image';
+
+interface PokemonDetailsClientProps {
+  id: string;
+}
+
+function PokemonDetailsClient({ id }: PokemonDetailsClientProps) {
+  const { data } = useFetchApi<PokemonId>({
+    url: `/pokemon/${id}`,
+    initialData: { id: 0, name: '', url: '', sprites: '', types: [] },
+  });
+
+  const light = typesLight[data.types[0]?.type.name];
+
+  return (
+    <div
+      className='flex justify-start my-4 bg-waves bg-contain bg-no-repeat h-full w-[70%] m-auto capitalize'
+      style={{
+        backgroundPosition: 'bottom',
+      }}
+    >
+      <div className='flex justify-center flex-col'>
+        <span className='text-2xl text-secondary-light'>{data.name}</span>
+        <div className='relative flex justify-start items-center'>
+          <div
+            className='absolute w-[300px] h-[300px] rounded-full'
+            style={{ backgroundColor: light }}
+          ></div>
+          <Image
+            className='z-20 translate'
+            src={getImageUrl(+id)}
+            width={400}
+            height={400}
+            alt={data.name}
+          />
+        </div>
+      </div>
+      <div>Info here</div>
+    </div>
+  );
+}
+
+export default PokemonDetailsClient;
