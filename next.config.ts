2a66f93b2e4662d2e8560808c5df03b4db2ea86c
Manoel Neto <mneto_004@hotmail.com>
2025-01-16 07:18:11 -0300

    chore: workflows

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/next-pokedex',
+  assetPrefix: '/next-pokedex',
   output: 'export',
   images: {
     domains: ['assets.pokemon.com'],
